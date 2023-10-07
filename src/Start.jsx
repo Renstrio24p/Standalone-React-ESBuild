802c8d70f0714825860adf0fb190a1726e7717fa
Waren Gador <waren.gador15@gmail.com>
2023-10-08 02:40:24 +0800

    add files

---
+++ src/Start.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactApp from "./components/react";
+
+export default function Start(){
+
+    return(
+        <div>
+            <ReactApp />
+            {/*You can start coding here..*/}
+        </div> 
+    )
+
+}
