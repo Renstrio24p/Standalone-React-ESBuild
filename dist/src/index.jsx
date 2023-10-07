853fee110f4f8089fd91b2c251253e33c20c7dc6
Waren Gador <waren.gador15@gmail.com>
2023-10-08 02:44:29 +0800

    add dist

---
+++ dist/src/index.jsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import Start from "./Start";
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+    <StrictMode>
+        <Start />
+    </StrictMode>
+)
